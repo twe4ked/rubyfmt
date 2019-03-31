6ca2a386734dd50ed33fe46dae41b7022149429b
George Brocklehurst <george@georgebrock.com>
2019-03-31 12:53:47 -0400

    Remove redundant exit status check.
    The script uses `set -e`, so it will already exit if the exit status of
    `diff` is anything other than zero.

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,11 +6,6 @@ test_folder() {
     do
         time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
         diff -u /tmp/out.rb $file
-        if [[ $? -ne 0 ]]
-        then
-            echo "got diff"
-            exit 1
-        fi
     done
 }
 
